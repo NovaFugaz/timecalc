fb46cd589e5991741e31c7c73419a552b24d39a2
Ignacia Parraguez <nacchi@outlook.cl>
2023-04-24 20:04:30 -0400

    Cambiado a tabla el cuerpo, arreglado algo del diseño base

---
+++ assets/js/clonfila.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var addRowBtn = document.getElementById('addRowBtn');
+    var lastRow = document.querySelector('.table tbody tr:last-child'); // variable para almacenar la última fila agregada
+    addRowBtn.addEventListener('click', function() {
+      // Clonar la última fila agregada
+      var newRow = lastRow.cloneNode(true);
+  
+      // Reiniciar los valores de los campos de entrada en la nueva fila
+      newRow.querySelectorAll('input').forEach(function(input) {
+        input.value = 0;
+      });
+  
+      // Insertar la nueva fila después de la última fila agregada
+      lastRow.parentNode.insertBefore(newRow, lastRow.nextSibling);
+  
+      // Actualizar la variable lastRow
+      lastRow = newRow;
+    });
+  });
